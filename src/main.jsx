6455b0a5c9a9b9989e799e0204aae2cda095e55b
Andrea Reyes <yuriramirez5800@gmail.com>
2025-02-12 00:18:04 -0500

    Se crea el login

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Dashboard } from './components/Dashboard.jsx'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Login from './pages/login.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/inicio" element={<div>Inicio</div>} />
-        <Route path="/planificador" element={<div>Planificador</div>} />
-        <Route path="/añadir-Metas" element={<div>Añadir Metas</div>} />
-        <Route path="/añadir-Recordatorios" element={<div>Añadir Recordatorios</div>} />
-      </Routes>
-    </Router>
+    
+    <Login />
+     
   </React.StrictMode>,
 )
